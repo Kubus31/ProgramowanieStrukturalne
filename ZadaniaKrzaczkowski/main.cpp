23c9fd400e717f3b35b38ded2b122a03de11b1bb
Kubus31 <jakubbazelak@gmail.com>
2022-05-25 23:29:27 +0100

    aktualizacja rozwiazan

--- ZadaniaKrzaczkowski/main.cpp
+++ ZadaniaKrzaczkowski/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 using namespace std;
+/*    Zadanie 2.2.1
+int WartoscBezwzgledna(int liczba) {
+	if (liczba > 0)
+		return liczba;
+	else
+		return liczba * (-1);
+}
 
 int main() {
-	
+	int n;
+	cout << "Podaj liczbe: "; cin >> n;
+	cout << "Wartosc bezwzgledna danej liczby wynosi: " << WartoscBezwzgledna(n);
 }
+*/
+
+
+
+/*   Zadanie 2.2.2
+long long silnia(long long liczba) {
+	long long wynik = 1;
+	for (liczba; liczba > 1 ; liczba--) {
+		wynik *= liczba;
+	}
+	return wynik;
+}
+
+int main(){
+	long long n;
+	cin >> n;
+	while (n <= 0)
+	{
+		if (n <= 0) {
+			cout << "Liczba musi byc wieksza od 0, podaj odpowiednia wartosc: "; cin >> n;
+		}
+	}
+	cout << silnia(n);
+}*/
